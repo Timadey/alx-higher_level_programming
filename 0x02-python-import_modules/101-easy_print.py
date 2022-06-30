e9eecb66847d4bffcb25b9d95c5df3a0ef375027
Timothy Adeleke <dlktimothy@gmail.com>
2022-06-30 19:44:31 +0000

    fix errors

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-import stdout from sys stdout.write("#pythoniscool\n")
+import easy_print_mod 
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    for name in (dir("hidden_4")):
+    import hidden_4
+    for name in (dir(hidden_4)):
         if "__" == name[:2]:
             continue
         print(f"{name}")
