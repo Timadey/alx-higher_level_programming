a6d614cd8c60dbe2c05804d4dd9a16021e06df8b
Timothy Adeleke <dlktimothy@gmail.com>
2022-07-04 12:55:29 +0000

    fix betty for 100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,14 +1,13 @@
 #include <Python.h>
 #include <stdio.h>
 /**
- * void print_python_list_info - prints some basic info about
+ * print_python_list_info - prints some basic info about
  * Python lists
- * @PyObject: a pointer to python list object
+ * @p: a pointer to python list object
  * Return: void
  */
 void print_python_list_info(PyObject *p)
 {
-	PyVarObject *list;
 	Py_ssize_t list_size;
 	Py_ssize_t allocated;
 	Py_ssize_t index;
@@ -17,28 +16,17 @@ void print_python_list_info(PyObject *p)
 
 	if (!PyList_Check(p))
 		return;
-	list_size = PyList_Size(p);
 
-	/** 
-	 * print size
-	 */
+	list_size = PyList_Size(p);
 	printf("[*] Size of the Python List = %ld\n", list_size);
-	
-	/**
-	 * Print allocated
-	 */
-	list = (PyVarObject *) p;
-	allocated = list->ob_size;
+
+	allocated = Py_SIZE(p);
 	printf("[*] Allocated = %ld\n", allocated);
 
 	for (index = 0; index < list_size; index++)
 	{
 		curr = PyList_GetItem(p, index);
 		curr_type = Py_TYPE(curr);
-		
-		/**
-		 * print item 
-		 */
 		printf("Element %ld: %s\n", index, curr_type->tp_name);
 	}
 }
