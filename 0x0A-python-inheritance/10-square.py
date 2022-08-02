ddd5ae0aa00ee171d9d91c074fa6d6677f10933a
Timadey <dlktimothy@gmail.com>
2022-08-02 16:10:21 +0100

    [Fix] Initialization error in Square class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -15,4 +15,6 @@ class Square(Rectangle):
     def __init__(self, size):
         """Instantiate with @size"""
 
-        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
