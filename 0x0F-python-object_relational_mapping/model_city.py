053612d1ab9df7e42b5b17b03f188eb15881be5d
Timadey <dlktimothy@gmail.com>
2022-10-08 15:17:08 +0100

    Fix 13 and 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-A City Model
+Contains the class definition of a City
 """
-
 from model_state import Base
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(Base):
     """
-    A City that links to MySQL table 'cities'
+    Class that defines each city
     """
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('state.id'), nullable=False, )
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
