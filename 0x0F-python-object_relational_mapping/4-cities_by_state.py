1f053e57278866f747e74721bfdb2f77ec5f6c28
Timadey <dlktimothy@gmail.com>
2022-10-08 15:07:17 +0100

    Edit task 4 and 5

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-lists all cities from the database hbtn_0e_4_usa
+List all cities from a database
 """
 import sys
-import MySQLdb as sql
+import MySQLdb
 
 if __name__ == '__main__':
-    db = sql.connect(user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
-         FROM cities JOIN state ON \
-            cities.state_id = states.id;")
+    FROM cities JOIN states ON cities.state_id = states.id;")
     states = cur.fetchall()
 
     for state in states:
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
- a script that takes in the name of a state as an argument
- and lists all cities of that state,
- using the database hbtn_0e_4_usa
+List all cities of a state
 """
-import MySQLdb as sql
 import sys
+import MySQLdb
 
 if __name__ == '__main__':
-    db = sql.connect(user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
-         FROM cities JOIN state ON cities.state_id = states.id \
-         WHERE state.name = %s;", (sys.argv[4],))
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
 
-    for state in states:
-        print(state)
+    print(", ".join([state[1] for state in states]))
