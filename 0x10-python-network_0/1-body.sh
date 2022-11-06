0f663c1d26c4bf62bb680324af9a17eb44ad0a5a
Timadey <dlktimothy@gmail.com>
2022-11-06 07:12:00 +0100

    add trailing line

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# senda a GET request and display the response
-
-status=$(curl "$1" -s -o /dev/null -w %{http_code});
-if [ "$status" -eq "200" ]
-then curl -Ls "$1";
-fi
-
-
+# send a a GET request and display the response
+curl -Ls "$1"
