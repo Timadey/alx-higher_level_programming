2631b4f7251cf2991472e437d6bfe1c3fd0179ab
Timadey <dlktimothy@gmail.com>
2023-01-08 00:59:23 +0100

    monosiproto task 101

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
-  if (res.statusCode === 200) {
-    for (const character of JSON.parse(body).characters) {
-      request.get(character, function (err1, res1, body1) {
-        if (res1.statusCode === 200) {
-          console.log(JSON.parse(body1).name);
-        } else if (err1) {
-          console.log(err1);
-        }
-      });
-    }
-  } else if (err) {
-    console.log(err);
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
+
+request(starWarsUri, function (_err, _res, body) {
+  const characters = JSON.parse(body).characters;
+
+  for (let i = 0; i < characters.length; ++i) {
+    request(characters[i], function (_cErr, _cRes, cBody) {
+      console.log(JSON.parse(cBody).name);
+    });
   }
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,46 @@
 #!/usr/bin/node
 
 const request = require('request');
-let characters;
-const dict = {};
-request.get('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
-  if (res.statusCode === 200) {
-    characters = JSON.parse(body).characters;
-    for (const url of characters) {
-      request.get(url, function (err1, res1, body1) {
-        if (res1.statusCode === 200) {
-          savedData(url, JSON.parse(body1).name);
-        } else if (err1) {
-          console.log(err1);
-        }
-      });
-    }
-  } else if (err) {
-    console.log(err);
-  }
-});
 
-function savedData (url, name) {
-  dict[url] = name;
-  if (Object.entries(dict).length === characters.length) {
-    for (const url of characters) {
-      console.log(dict[url]);
-    }
-  }
+function getDataFrom (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, _res, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
 }
+
+function errHandler (err) {
+  console.log(err);
+}
+
+function printMovieCharacters (movieId) {
+  const movieUri = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+  getDataFrom(movieUri)
+    .then(JSON.parse, errHandler)
+    .then(function (res) {
+      const characters = res.characters;
+      const promises = [];
+
+      for (let i = 0; i < characters.length; ++i) {
+        promises.push(getDataFrom(characters[i]));
+      }
+
+      Promise.all(promises)
+        .then((results) => {
+          for (let i = 0; i < results.length; ++i) {
+            console.log(JSON.parse(results[i]).name);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+}
+
+printMovieCharacters(process.argv[2]);
