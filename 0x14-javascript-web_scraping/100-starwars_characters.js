2fcf64808c3bb6a8240ab613759b79afc86b29cd
Timadey <dlktimothy@gmail.com>
2023-01-08 00:46:41 +0100

    update advanced task

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
+const request = require('request');
+request.get('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
+  if (res.statusCode === 200) {
+    for (const character of JSON.parse(body).characters) {
+      request.get(character, function (err1, res1, body1) {
+        if (res1.statusCode === 200) {
+          console.log(JSON.parse(body1).name);
+        } else if (err1) {
+          console.log(err1);
         }
       });
     }
+  } else if (err) {
+    console.log(err);
   }
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,30 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi-api.alx-tools.com/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
+const request = require('request');
+let characters;
+const dict = {};
+request.get('http://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
+  if (res.statusCode === 200) {
+    characters = JSON.parse(body).characters;
+    for (const url of characters) {
+      request.get(url, function (err1, res1, body1) {
+        if (res1.statusCode === 200) {
+          savedData(url, JSON.parse(body1).name);
+        } else if (err1) {
+          console.log(err1);
         }
       });
     }
+  } else if (err) {
+    console.log(err);
   }
 });
+
+function savedData (url, name) {
+  dict[url] = name;
+  if (Object.entries(dict).length === characters.length) {
+    for (const url of characters) {
+      console.log(dict[url]);
+    }
+  }
+}
